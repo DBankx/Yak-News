2215b670581285993f0d1ee8dc9c219d0fdb4ef7
DBankx <61208493+DBankx@users.noreply.github.com>
2020-07-10 05:13:34 +0100

    initialized redux into the application & created a store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <Provider store={store}>
+      <div className='App'>Hello World</div>
+    </Provider>
+  );
 }
 
 export default App;
