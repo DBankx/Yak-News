6106c3c2dd5b6fd21de3055395c5e3a6be378f53
DBankx <61208493+DBankx@users.noreply.github.com>
2020-07-11 19:28:51 +0100

    placed the buttons to the sides of the carousel and improved the ui

--- src/components/tabviews/Carousel.js
+++ src/components/tabviews/Carousel.js
@@ -5,65 +5,76 @@ import {
   Slider,
   Slide,
   ButtonBack,
-  ButtonNext
+  ButtonNext,
+  Image,
+  DotGroup
 } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
-import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import Spinner from '../layout/Spinner';
 
 const Carousel = ({ data }) => {
   const mainData = data && data;
 
   return (
     <CarouselProvider
-      naturalSlideWidth={100}
-      naturalSlideHeight={isTablet ? 40 : 30}
+      naturalSlideWidth={80}
+      naturalSlideHeight={isTablet ? 40 : 25}
       interval={4000}
       //   gets the length of the data array
       totalSlides={data && mainData.length}
+      className='hide-sm'
     >
-      {/* map through the data and render it in the ui */}
-      <Slider className='carousel-slider'>
-        {data &&
-          mainData.map((news, index) => {
-            return (
-              <Slide key={news.id} index={index}>
-                <a href={news.url} target='_blank' rel='noopener noreferrer'>
-                  <div className='carousel-news'>
-                    <img
-                      src={news.media[0]['media-metadata'][2].url}
-                      alt='headline'
-                    />
-                    <div className='news-body'>
-                      <h1>{news.title}</h1>
-                      <p className='abstract'>{news.abstract}</p>
-                      <p className='byline'>{news.byline}</p>
-                      <ul>
-                        <li>
-                          <QueryBuilderIcon />
-                        </li>
-                        <li>
-                          <strong>
-                            <Moment fromNow>{news.updated}</Moment>
-                          </strong>
-                        </li>
-                      </ul>
+      <div className='carousel-body'>
+        <ButtonBack className='trig-button'>
+          <KeyboardArrowLeftIcon />
+        </ButtonBack>
+        {/* map through the data and render it in the ui */}
+        <Slider className='carousel-slider'>
+          {data ? (
+            mainData.map((news, index) => {
+              return (
+                <Slide key={news.id} index={index}>
+                  <a href={news.url} target='_blank' rel='noopener noreferrer'>
+                    <div className='carousel-news'>
+                      <Image
+                        src={news.media[0]['media-metadata'][2].url}
+                        alt='headline'
+                        hasMasterSpinner={true}
+                      />
+                      <div className='news-body'>
+                        <h1>{news.title}</h1>
+                        <p className='abstract'>{news.abstract}</p>
+                        <p className='byline'>{news.byline}</p>
+                        <ul>
+                          <li>
+                            <QueryBuilderIcon />
+                          </li>
+                          <li>
+                            <strong>
+                              <Moment fromNow>{news.updated}</Moment>
+                            </strong>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
-                  </div>
-                </a>
-              </Slide>
-            );
-          })}
-      </Slider>
-      <ButtonBack style={{ backgroundColor: 'green' }}>
-        <KeyboardArrowLeftIcon />
-      </ButtonBack>
-      <ButtonNext style={{ color: 'green' }}>
-        <KeyboardArrowRightIcon />
-      </ButtonNext>
+                  </a>
+                </Slide>
+              );
+            })
+          ) : (
+            <Spinner className='carousel-spinner' />
+          )}
+        </Slider>
+
+        <ButtonNext className='trig-button'>
+          <KeyboardArrowRightIcon />
+        </ButtonNext>
+      </div>
+      <DotGroup className='dot-group' />
     </CarouselProvider>
   );
 };
